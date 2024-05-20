4251b4133d3d1a41f05a1ff1353c79c20e5ac9d5
Sanjeev Rajbhandari <sanzeevstewart@gmail.com>
2024-05-20 07:14:20 +0100

    fixes

--- components/product/ProductWrapper.vue
+++ components/product/ProductWrapper.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <div class="product-area pb-90">
-      <div class="container">
-        <div v-if="$store.state.categories.length > 0">
+      <div class="container" v-if="$store.state.categories[0] != null">
+      <!-- v-if="$store.state.categories.length > 0" -->
+        <div >
 
           <SectionTitleWithSubTitle :title="`${$store.state.categories[0].category_name}'s Latest Products`"
             classes="section-title" />
